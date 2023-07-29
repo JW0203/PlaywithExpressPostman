62c0540a6f50115ee6a7ba1b9dc74830abb6e11c
JW0203 <widwoo0203@gmail.com>
2023-07-29 18:48:13 +0900

    refaxtor: remove unused lines of codes

--- practice-RESTAPI-Board/app.js
+++ practice-RESTAPI-Board/app.js
@@ -5,11 +5,8 @@ const boardsRouter = require("./routes/boards");
 // const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 //
-//
-// // https://www.zerocho.com/category/NodeJS/post/579b3fc4062e76a002648af6
-// app.use(methodOverride()); // PUT, DELETE를 지원 안 하는 클라이언트를 위해
 app.use(bodyParser.json()); // body의 데이터를 json형식으로 받음
-// app.use(bodyParser.urlencoded({ extended: true })); // qs모듈로 쿼리스트링 파싱
+
 
 
 app.use("/boards", boardsRouter);
